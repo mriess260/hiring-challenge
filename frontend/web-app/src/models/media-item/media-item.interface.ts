bc20e2ee9427a11c094f09891eb251ab47a654f5
Michael <michael.riess@mailfence.com>
2018-04-28 12:39:48 -0400

    Rebuilt add-media form using FormBuilder, and added validation on field entry and submission

--- frontend/web-app/src/models/media-item/media-item.interface.ts
+++ frontend/web-app/src/models/media-item/media-item.interface.ts
@@ -1,5 +1,7 @@
 export interface MediaItem {
   $key?: string;
   title: string;
+  descript: string;
   url: string;
+  thmbnl_url: string;
 }
--- frontend/web-app/src/pages/add-media/add-media.ts
+++ frontend/web-app/src/pages/add-media/add-media.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+//------------
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
-
+import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
+//------------
 import { MediaItem } from '../../models/media-item/media-item.interface';
-
+//------------
 
 @Component({
   selector: 'page-add-media',
@@ -12,26 +15,65 @@ import { MediaItem } from '../../models/media-item/media-item.interface';
 export class AddMediaPage {
 
   //create new object
-  mediaItem = {} as MediaItem
+  mediaItem = {} as MediaItem;
 
   //database observable reference
-  mediaItemRef$: FirebaseListObservable<MediaItem[]>
+  mediaItemRef$: FirebaseListObservable<MediaItem[]>;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,
-  private database: AngularFireDatabase) {
-    this.mediaItemRef$ = this.database.list('media-list');
+  formGroup: FormGroup;
+  //validationMessages: ValidationMessages;
+
+  //form validation error messages
+  validationMessages = {
+  'title': [
+  		{ type: 'required', message: 'Title is required.' },
+  		{ type: 'minlength', message: 'Title must be at least 1 character(s) long.' },
+  		{ type: 'maxlength', message: 'Title cannot be more than 25 characters long.' }
+  	],
+  	'url': [
+  		{ type: 'required', message: 'Media URL/Link is required.' },
+      { type: 'minlength', message: 'URL must be at least 10 character(s) long.' },
+      { type: 'pattern', message: 'Must be a valid URL.' },
+  	],
+    'descript': [
+        { type: 'maxlength', message: 'Desciption cannot be more than 1024 characters long.' }
+    ],
+    'thmbnl_url': [
+      { type: 'required', message: 'Thumbnail URL is required.' },
+      { type: 'minlength', message: 'URL must be at least 10 character(s) long.' },
+      { type: 'pattern', message: 'Must be a valid URL.' },
+    ]
   }
 
-  addMediaItem(mediaItem: MediaItem){
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+  private database: AngularFireDatabase, public formBuilder: FormBuilder) {
 
+    this.formGroup = formBuilder.group({
+      title:['', Validators.compose([Validators.minLength(1), Validators.required])],
+      url:['', Validators.compose([
+        Validators.pattern('^(?!mailto:)(?:(?:http|https|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localhost)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?$'),
+        Validators.minLength(10),
+          Validators.required
+      ])],
+      descript:['', Validators.maxLength(1024)],
+      thmbnl_url:['', Validators.compose([
+        Validators.pattern('^(?!mailto:)(?:(?:http|https|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localhost)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?$'),
+        Validators.minLength(10),
+        Validators.required
+      ])]
+    });
+
+    this.mediaItemRef$ = this.database.list('media-list');
+
+  }
+
+
+  addMediaItem(mediaItem: MediaItem){
     // create new anonymous object and push this to firebase under media-list node
     this.mediaItemRef$.push({
       title: this.mediaItem.title,
       url: this.mediaItem.url
     });
-
-    //<<for testing>>
-    console.log(mediaItem);
 
     //Clear mediaItem
     this.mediaItem = {} as MediaItem;
